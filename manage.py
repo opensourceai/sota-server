2e3541c92625e7e01a4ceb290ede4a448a49b7ed
ChenQuan <chenquan@osai.club>
2019-07-15 11:29:42 +0800

    test

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sota_server.settings')
     try:
